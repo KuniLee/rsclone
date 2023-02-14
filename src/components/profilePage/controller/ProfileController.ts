91196b7f3b9b2e96b99fc994de5d19dd73933ec6
4k1r1n <arinakire@gmail.com>
2023-02-14 16:06:59 +0300

    feat: add preloader

--- src/components/profilePage/controller/ProfileController.ts
+++ src/components/profilePage/controller/ProfileController.ts
@@ -1,5 +1,7 @@
+import { UserData } from 'types/types'
 import { PageModelInstance } from '@/components/mainPage/model/PageModel'
-import { FireBaseAPIInstance } from '@/utils/FireBaseAPI'
+import { doc, FireBaseAPIInstance, Firestore, getDoc } from '@/utils/FireBaseAPI'
+import { Auth, getAuth, User } from 'firebase/auth'
 import { ProfileModelInstance } from '../model/ProfileModel'
 import { ProfileViewInstance } from './../view/ProfileView'
 
@@ -7,14 +9,43 @@ export class ProfileController {
     private view: ProfileViewInstance
     private profileModel: ProfileModelInstance
     private pageModel: PageModelInstance
+    private db: Firestore
+    private auth: Auth
 
     constructor(
         view: ProfileViewInstance,
         private models: { profileModel: ProfileModelInstance; pageModel: PageModelInstance },
         private api: FireBaseAPIInstance
     ) {
+        this.db = api.db
+        this.auth = api.auth
         this.view = view
         this.pageModel = models.pageModel
         this.profileModel = models.profileModel
+        this.view.on('LOAD_USER_INFO', async (username: User['displayName']) => {
+            const userInfo = await this.getUserInfo(username)
+            this.profileModel.setUserInfo = userInfo
+        })
+    }
+
+    get currentUser() {
+        const auth = getAuth()
+        return auth.currentUser
+    }
+
+    private async getUserInfo(username: User['displayName']) {
+        const user = this.currentUser
+        if (user) {
+            const uid = user.uid
+            if (user && user.displayName === username) {
+                const docRef = doc(this.db, `users/${uid}`)
+                try {
+                    const docSnap = await getDoc(docRef)
+                    return { ...docSnap.data() } as UserData
+                } catch (error) {
+                    console.log('Error fetching user data:', error)
+                }
+            }
+        }
     }
 }
